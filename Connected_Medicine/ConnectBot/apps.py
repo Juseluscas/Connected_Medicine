c174b6d1ed29c5bb2023833c9f99825467bdb499
Juseluscas <Jos3.lucasjlp@gmail.com>
2020-04-20 13:00:50 -0300

    framework django

---
+++ Connected_Medicine/ConnectBot/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ConnectbotConfig(AppConfig):
+    name = 'ConnectBot'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jose.pereira\desktop\connected_medicine\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
